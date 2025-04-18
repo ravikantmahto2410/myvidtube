0df74031b922557deb35905256cff176bc9c1f0a
ravikantmahto2410 <office.rakamahto2002@gmail.com>
2025-04-18 06:33:42 +0530

    create health check by using best practices

---
+++ src/controllers/healthcheck.controller.js
@@ -0,0 +1,25 @@
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asynchandler.js";
+
+//create healthcheck
+    //usual common practise
+    // const healthcheck = async (req, res) => {
+    //     //there is no guarantee that req, res will happen as expected, so every single time we have to write a try catch block
+        
+    //     try {
+    //         res.status(200).json
+    //     } catch (error) {
+            
+    //     }
+        
+    // }
+
+    //but since i don't want to use this do try catch every single time ,I have an an asyncHandler built up
+    const helathcheck = asyncHandler( async(req , res) => {
+        return res
+        .status(200)
+        // .json( {message: "test ok"}) //but this is not an standard response , the standerd will be that we have done inApiresponse
+        .json(new ApiResponse(200, "OK", "Health check passed"))
+
+    })
+    export {helathcheck}
