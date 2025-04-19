ae2633ff8ae5b4c039d4ec140c86834c9df157d2
ravikantmahto2410 <office.rakamahto2002@gmail.com>
2025-04-19 18:55:00 +0530

    create Video model

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,16 +1,3 @@
-/*
-    id string pk 
-    username string
-    email string 
-    fullName string
-    avatar string
-    coverImage string
-    watchHistory ObjectId[] videos 
-    password string 
-    refreshToken string 
-    createdAt Date 
-    updatedAt Date  
-*/
 
 import mongoose , { Schema } from "mongoose"
 
---
+++ src/models/video.models.js
@@ -0,0 +1,41 @@
+import mongoose , { Schema } from "momgoose"
+
+const videoSchema = new Schema(
+    {
+        videofile: {
+            type: String,  // it's a cloudinary url
+            required: true
+        },
+        thumbnail: {
+            type: String,  //It's a cloudinary url
+            required: true
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String, 
+            required: true
+        },
+        duration: {
+            type: Number,
+            required: true
+        },
+        views: {
+            type: Number,
+            default: 0
+        },
+        isPublished: {
+            type: Boolean,
+            default: true
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+         
+    }, {timestamps: true}
+)
+
+export const Video = mongoose.model("Video", videoSchema)
