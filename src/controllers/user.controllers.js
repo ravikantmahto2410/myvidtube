fe110ad3ed4992d782655a3c0c4134adeed58cd4
ravikantmahto2410 <office.rakamahto2002@gmail.com>
2025-05-01 16:49:13 +0530

    Routing for User is done

---
+++ src/controllers/user.controllers.js
@@ -0,0 +1,9 @@
+import { asyncHandler } from "../utils/asynchandler"
+
+const registerUser =  asyncHandler(async (req, res) => {
+    //TODO
+})
+
+export {
+    registerUser
+}
---
+++ src/routes/user.routes.js
@@ -0,0 +1,25 @@
+
+////note each model will have it's own controller and each controller will have its routes
+
+import { Router } from "express"
+import { registerUser } from "../controllers/user.controllers.js"
+import { upload } from "../middlewares/multer.middlewares.js"
+
+const router = Router()
+
+router.route("/register").post(  //where this router will serve the route that is .route("/register") .     //this is a post request because we are receiving some data and in the post request we need a controller to be brought in and that will be registerUser // But there is one issue with this , that is we need to uplaod or get some upload from the user  
+    upload.fields([
+        {
+            name: "avatar",  //this name is important because that's how you are accepting the file in the controller itself
+            maxCount: 1
+        },{
+            name: "coverImage",
+            maxCount: 1,
+        }
+
+    ]),
+    registerUser 
+) //where this router will serve the route that is .route("/register") .     //this is a post request because we are receiving some data and in the post request we need a controller to be brought in and that will be registerUser // But there is one issue with this , that is we need to uplaod or get some upload from the user  
+
+export default router
+
