0862c282c3aef0506ac5bfb2d2be3de32be10ff3
ravikantmahto2410 <office.rakamahto2002@gmail.com>
2025-05-19 01:08:03 +0530

    Update coverIMage route working absolutely fine

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -33,7 +33,7 @@ router.route("/current-user").get(verifyJWT, getCurrentUser)//current user
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
 router.route("/update-account").patch(verifyJWT, updateAccountDetails)//route to update the account
 router.route("/avatar").patch(verifyJWT,upload.single("avatar"), updateUserAvatar) //patch to update the avatar //working fine
-router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"), updateUserCoverImage) //patch to update the avatar
+router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"), updateUserCoverImage) //patch to update the avatar // coverImage Update route is working fine
 router.route("/history").get(verifyJWT, getWatchHistory)
 
 export default router
