97234fc889c780a2710103e44fb0696f0ad0b464
ravikantmahto2410 <office.rakamahto2002@gmail.com>
2025-08-26 18:25:52 +0530

    write controllers for getVideoComments

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -4,11 +4,42 @@ import { ApiResponse } from "../utils/ApiResponse.js"
 import { ApiError } from "../utils/ApiError.js"
 import { Comment } from "../models/comment.models.js"
 
+
 const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
     const {videoId} = req.params
     const {page = 1, limit = 10} = req.query
 
+    if(!videoId || !mongoose.Types.ObjectId.isValid(videoId)){
+        throw new ApiError(400, "videoId is invalid")
+    }
+
+    const comment =  Comment.aggregate([
+        {
+            $match : {
+               video: new mongoose.Types.ObjectId(videoId)
+            }
+        },
+        {
+            $sort:{
+                createdAt: -1
+            }
+        }
+    ])
+
+    const paginate = await Comment.aggregatePaginate(comment,{
+
+        page:parseInt(page),
+        limit: parseInt(limit)
+    })
+
+    return res
+        .status(200)
+        .json(new ApiResponse(
+            200,
+            paginate,
+            "video Comment Fetched Successfully"
+        ))
 })
 
 const addComment = asyncHandler(async (req, res) => {
@@ -45,6 +76,40 @@ const addComment = asyncHandler(async (req, res) => {
 
 const updateComment = asyncHandler(async (req, res) => {
     // TODO: update a comment
+    const {commentId} = req.params
+    const {content} = req.body
+
+    if(!commentId || !mongoose.Types.ObjectId.isValid(commentId)){
+        throw new ApiError(400, "CommentId not found")
+    }
+
+    if(!content?.trim()){
+        throw new ApiError(401, "Content is required")
+    }
+
+    
+    const existingComment = await Comment.findById(commentId)
+    if(!existingComment){
+        throw new ApiError(404, "comment not found")
+    }
+
+    if(existingComment.owner.toString() !== req.user?._id.toString()){
+        throw new ApiError(403, "You are not authorized to delete this comment")
+    }
+
+    const updatedComment = await Comment.findByIdAndUpdate(
+        commentId,
+        {
+            $set :{
+                content: content
+            }
+        },
+        {new:true}
+    )
+
+    return res
+        .status(200)
+        .json(new ApiResponse (200, updatedComment, "comment updated successfully"))
 })
 
 const deleteComment = asyncHandler(async (req, res) => {
--- src/routes/comment.route.js
+++ src/routes/comment.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { addComment, deleteComment } from "../controllers/comment.controller.js";
+import { addComment, deleteComment,getVideoComments,updateComment} from "../controllers/comment.controller.js";
 
 
 const router = Router()
@@ -8,5 +8,7 @@ const router = Router()
 
 router.route('/addcomment/:videoId').post(verifyJWT, addComment)
 router.route('/deletecomment/:commentId').delete(verifyJWT, deleteComment)
+router.route('/update-comment/:commentId').patch(verifyJWT,updateComment)
+router.route('/getvideocomments/:videoId').get(verifyJWT, getVideoComments)
 
 export default router
